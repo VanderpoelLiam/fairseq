47a18621f30dcc468c68d14a9025d193d978d49e
liam <lvander@student.ethz.ch>
2022-03-28 16:25:05 +0200

    Got basic scoring working

--- fairseq_cli/generate.py
+++ fairseq_cli/generate.py
@@ -317,6 +317,8 @@ def _main(cfg: DictConfig, output_file):
                     # print(lm_scores)
                     # print(hypo_str)
 
+                    # TODO: Why is full_scores 1 token longer than lm_scores?
+
                     t1 = full_scores[:-1].cpu().detach().numpy()
                     t2 = cfg.generation.lm_weight * lm_scores.cpu().detach().numpy()
                     sm_scores = (t1 - t2)
